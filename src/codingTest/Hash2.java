e587c9c1dcb72b9d68299b9ea4371343c0d473fc
Oh,Jiwon <ojw970725@gmail.com>
2020-11-16 01:17:56 +0900

    201115 전화번호 목록

--- src/codingTest/Hash2.java
+++ src/codingTest/Hash2.java
@@ -6,13 +6,13 @@ public class Hash2 {
 
 	public static boolean solution(String[] phone_book) {
 	boolean answer = true;
-    int num=phone_book[0].length();
 
-    for(int i=1;i<phone_book.length;i++){
-        String sub=phone_book[i].substring(0,num);
-        if(phone_book[0].equals(sub)) answer=false;
-
-    }
+	for(int i=0;i<phone_book.length;i++) {
+		for(int j=i+1;j<phone_book.length;j++) {
+			if(phone_book[i].startsWith(phone_book[j])) return false;
+			if(phone_book[j].startsWith(phone_book[i])) return false;
+		}
+	}
 
     return answer;
 
