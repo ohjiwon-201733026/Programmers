526916f3fc08120199bb5961d2ad710602d89460
OH JIWON <ojw970725@gmail.com>
2020-11-24 23:32:53 +0900

    201124 백준 좌표정렬11650

--- src/BaekJoon/Sort/B11650.java
+++ src/BaekJoon/Sort/B11650.java
@@ -5,49 +5,42 @@ import java.util.Comparator;
 import java.util.Scanner;
 
 public class B11650 {
+	public static class Point{
+		int x;
+		int y;
+
+		public Point(int x, int y) {
+			this.x=x;
+			this.y=y;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		Point [] points=new Point[n];
+		Point[] points=new Point[n];
 		for(int i=0;i<n;i++) {
-			String x=sc.next();
-			String y=sc.next();
+			int x=sc.nextInt();
+			int y=sc.nextInt();
 			points[i]=new Point(x,y);
 		}
 
-		Point[] result=solution(points);
-		for(int i=0;i<result.length;i++) {
-			System.out.println(result[i].x+" "+result[i].y);
-		}
-
-	}
-
-	public static class Point{
-		String x;
-		String y;
-
-		public Point(String x,String y) {
-			this.x=x;
-			this.y=y;
-		}
-
-	}
-
-	public static Point[] solution(Point[] array){
-		Arrays.sort(array,new Comparator<Point>() {
+		Arrays.sort(points,new Comparator<Point>() {
 			@Override
 			public int compare(Point o1, Point o2) {
 				// TODO Auto-generated method stub
-				String p1X=o1.x;
-				String p1Y=o1.y;
-				return p1X.compareTo(o2.x)==0?p1Y.compareTo(o2.y):p1X.compareTo(o2.x);
+//				if(o1[0]==o2[0]) return Integer.compare(o1[1], o2[1]);
+//				return Integer.compare(o1[0],o2[0]);
+				return Integer.compare(o1.x,o2.x)==0?Integer.compare(o1.y,o2.y):Integer.compare(o1.x,o2.x);
 			};
 		});
 
-
-		return array;
-
+		for(int i=0;i<points.length;i++) {
+			System.out.println(points[i].x+" "+points[i].y);
 		}
+
+	}
+
+
 }
---
+++ src/BaekJoon/Sort/B1181.java
@@ -0,0 +1,11 @@
+package BaekJoon.Sort;
+
+public class B1181 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+
+	}
+
+}
