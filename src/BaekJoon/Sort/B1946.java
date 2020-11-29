8ea57ac36ee85a3e3de74c333aa535ac2e78eded
OH JIWON <ojw970725@gmail.com>
2020-11-29 23:10:11 +0900

    201129 백준 신입사원

--- src/BaekJoon/Sort/B1946.java
+++ src/BaekJoon/Sort/B1946.java
@@ -1,66 +1,63 @@
 package BaekJoon.Sort;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class B1946 {
-	public static class Person{
+	public static class Person implements Comparable<Person>{
 		int su;
 		int me;
-		boolean flag;
 
 		public Person(int su, int me) {
 			this.su=su;
 			this.me=me;
-			this.flag=true;
 		}
-	}
-
-	public static class PersonSuComparator implements Comparator<Person>{
 
 		@Override
-		public int compare(Person o1, Person o2) {
+		public int compareTo(Person o) {
 			// TODO Auto-generated method stub
-			return o1.su-o2.su;
+			return this.su-o.su;
 		}
-
 	}
 
+
+	public static List<Person> inputSortedEmployee(){
+
+		int num=sc.nextInt();
+		List<Person> persons=new ArrayList<>();
+		for(int i=0;i<num;i++) {
+			int su=sc.nextInt();
+			int me=sc.nextInt();
+			persons.add(new Person(su,me));
+		}
+		Collections.sort(persons);
+		return persons;
+	}
+
+	public static int solve(List<Person> persons) {
+		int cnt=0;
+		int minInterViewRank=Integer.MAX_VALUE;
+
+		for(Person p:persons) {
+			if(p.me<minInterViewRank) {
+				cnt++;
+				minInterViewRank=p.me;
+			}
+		}
+		return cnt;
+	}
+	static Scanner sc=new Scanner(System.in);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
+
+
 		int testCase=sc.nextInt();
-		Person [][] persons=new Person[testCase][];
 
 		for(int i=0;i<testCase;i++) {
-			int n=sc.nextInt();
-			persons[i]=new Person[n];
-
-			for(int j=0;j<n;j++) {
-				int su=sc.nextInt();
-				int me=sc.nextInt();
-				persons[i][j]=new Person(su,me);
-			}
-
-
-			Arrays.sort(persons[i],new PersonSuComparator());
-			int count=persons[i].length;
-			for(int j=persons[i].length-1;j>=0;j--) {
-				for(int x=j-1;x>=0;x--) {
-					Person tmp=persons[i][j];
-					if(tmp.me>persons[i][x].me && persons[i][x].flag==true) {
-						--count;
-						persons[i][x].flag=false;
-					}
-				}
-			}
-
-			System.out.println(count);
-
-			for(int j=0;j<persons[i].length;j++) {
-				System.out.println(persons[i][j].su+" "+persons[i][j].me);
-		}
+			List<Person> persons=inputSortedEmployee();
+			System.out.println(solve(persons));
 		}
 
 	}
