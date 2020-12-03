563234d1e1e337a259aa62605f308d2921c3d1e8
OH JIWON <ojw970725@gmail.com>
2020-12-04 00:10:40 +0900

    201203 백준 스택 구현

---
+++ src/BaekJoon/Stack/B10828.java
@@ -0,0 +1,18 @@
+package BaekJoon.Stack;
+
+import java.util.Scanner;
+
+public class B10828 {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++) {
+			System.out.println(sc.next());
+		}
+
+	}
+
+}
