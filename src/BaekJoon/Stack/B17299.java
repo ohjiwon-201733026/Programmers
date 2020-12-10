fd4ec60792405f0eea4b3612690cc932d445484d
OH JIWON <ojw970725@gmail.com>
2020-12-10 20:05:22 +0900

    ****201210 오등큰수

--- src/BaekJoon/Stack/B17299.java
+++ src/BaekJoon/Stack/B17299.java
@@ -1,40 +1,58 @@
+//오등큰수
 package BaekJoon.Stack;
 
-import java.util.HashMap;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Stack;
 
 public class B17299 {
-	public class Number{
-		int key;
-		int count;
 
-		public Number(int key) {
-			this.key=key;
-			this.count=0;
-		}
-	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		Integer [] arr=new Integer[n];
-		for(int i=0;i<n;i++) {
-			arr[i]=sc.nextInt();
-		}
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
 
-		HashMap<Integer, Integer> hm=new HashMap<>();
-		for(int num:arr) {
-			hm.put(num, hm.getOrDefault(num,0)+1);
-		}
+		int size=Integer.parseInt(br.readLine());
+		String [] temp=br.readLine().split(" ");
+		int [] numbers=new int[size];
+		int [] counts=new int[1000001];
+		int [] answer=new int [size];
+
+		for(int i=0;i<size;i++) {
+			numbers[i]=Integer.parseInt(temp[i]);
+			counts[numbers[i]]+=1;
+			}
 
 		Stack<Integer> stack=new Stack<>();
-		stack.push(arr[0]);
-		for(int i=1;i<n;i++) {
-			if(hm.getValue(arr[1]))
+
+		stack.push(0);
+
+		for(int i=1;i<size;i++) {
+			if(stack.isEmpty()) {
+				stack.push(i);
+			}
+
+			while(!stack.isEmpty()&&counts[numbers[i]]>counts[numbers[stack.peek()]]) {
+				answer[stack.pop()]=numbers[i];
+			}
+			stack.push(i);
+		}
+		while(!stack.isEmpty()) {
+			answer[stack.pop()]=-1;
 		}
 
+		for(int i=0;i<size;i++) {
+			bw.write(String.valueOf(answer[i]+" "));
+		}
+		bw.write("\n");
+		bw.flush();
+
+		bw.close();
+		br.close();
 	}
 
 }
