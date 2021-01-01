15f145fd7f3eb6ca80d9d295fb12b0d6724a905e
OH JIWON <ojw970725@gmail.com>
2021-01-01 22:50:05 +0900

    210101 비슷한 단어

---
+++ src/BaekJoon/B2607.java
@@ -0,0 +1,21 @@
+// 비슷한 단어
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B2607 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		String word=sc.next();
+		String [] words=new String[n-1];
+
+		for(int i=0;i<n-1;i++) {
+			words[i]=sc.next();
+		}
+
+	}
+
+}
