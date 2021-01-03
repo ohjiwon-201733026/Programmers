09659c9fdd1a4691d74928075a23a616119853ec
OH JIWON <ojw970725@gmail.com>
2021-01-03 12:53:56 +0900

    210103 비슷한 단어**

--- src/BaekJoon/B2607.java
+++ src/BaekJoon/B2607.java
@@ -1,46 +1,67 @@
 // 비슷한 단어
 package BaekJoon;
 
-import java.util.Scanner;
-import java.util.Vector;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
 
 public class B2607 {
 
-	public static void main(String[] args) {
+	public static HashMap<Character, Integer> origin, other;
+
+	public static void init(HashMap<Character,Integer> hm) {
+		for(char ch='A';ch<='Z';ch++)
+			hm.put(ch, 0);
+	}
+
+	public static boolean isSimilarTo(HashMap<Character,Integer> hm) {
+		int a,b,diff=0,originLen = 0,otherLen=0;
+		for(char ch='A';ch<='Z';ch++) {
+			originLen+=(a=origin.get(ch));
+			otherLen+=(b=other.get(ch));
+			diff+=Math.abs(a-b);
+		}
+		return diff<=2&&Math.abs(originLen-otherLen)<=1;
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		String pattern=sc.next();
-		String [] words=new String[n-1];
-		int [] count=new int[n-1];
-		Vector<Character> v=new Vector<>();
-		int answer=0;
 
-		for(int i=0;i<pattern.length();i++) {
-			v.add(pattern.charAt(i));
+		BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out=new BufferedWriter(new OutputStreamWriter(System.out));
+
+		char ch;
+		int n=Integer.parseInt(in.readLine())-1;
+		String word=in.readLine();
+		int len=word.length();
+		int cnt=0;
+
+		origin=new HashMap<>();
+		other=new HashMap<>();
+
+		init(origin);
+		for(int i=0;i<len;i++) {
+			ch=word.charAt(i);
+			origin.put(ch, origin.get(ch)+1);
 		}
 
-		for(int i=0;i<n-1;i++) {
-			words[i]=sc.next();
-		}
-
-		for(int i=0;i<n-1;i++) {
-			for(int j=0;j<words[i].length();j++) {
-				if(v.contains(words[i].charAt(j))) {
-					count[i]++;
-
-				}
+		for(int i=0;i<n;i++) {
+			init(other);
+			word=in.readLine();
+			len=word.length();
+			for(int j=0;j<len;j++) {
+				ch=word.charAt(j);
+				other.put(ch, other.get(ch)+1);
 			}
+			if(isSimilarTo(other)) cnt++;
 		}
 
-		for(int i=0;i<count.length;i++) {
-			if(Math.abs(count[i]-pattern.length())<=1) {
-				System.out.println(words[i]);
-				answer++;
-			}
-		}
-
-		System.out.println(answer);
+		out.write(cnt+"");
+		out.close();
+		in.close();
 
 	}
 
