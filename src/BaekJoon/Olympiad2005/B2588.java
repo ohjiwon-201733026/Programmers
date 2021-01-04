ebf89a111057f367ec7af03fe0b6ae7b53c1304a
OH JIWON <ojw970725@gmail.com>
2021-01-04 12:47:16 +0900

    210114 곱셈

--- src/BaekJoon/Olympiad2005/B2588.java
+++ src/BaekJoon/Olympiad2005/B2588.java
@@ -1,10 +1,30 @@
 //곱셈
 package BaekJoon.Olympiad2005;
 
+import java.util.Scanner;
+
 public class B2588 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String a=sc.next();
+		String b=sc.next();
+
+		int [] arr=new int[4];
+		int count=0;
+		for(int i=a.length()-1;i>=0;i--) {
+			int c=Integer.parseInt(a)*(b.charAt(i)-'0');
+			System.out.println(c);
+			arr[i]=(int) (c*Math.pow(10, count));
+			count++;
+		}
+		int answer=0;
+		for(int i=0;i<arr.length-1;i++) {
+			answer+=arr[i];
+
+		}
+		System.out.println(answer);
 
 	}
 
