d100742b979afed4bc4dc50b9203deba445a7915
OH JIWON <ojw970725@gmail.com>
2021-01-13 18:32:58 +0900

    210113 스도쿠

--- src/BaekJoon/Olympiad2006/B2580.java
+++ src/BaekJoon/Olympiad2006/B2580.java
@@ -2,109 +2,86 @@
 package BaekJoon.Olympiad2006;
 
 import java.util.Scanner;
-import java.util.Vector;
 
 public class B2580 {
+	private static int [][] arr=new int [9][9];
 
-	private static boolean rowCheck(int [][] sdc, int x, int y) {
-		Vector<Integer> v=new Vector<>();
+	private static boolean possibility(int row, int col, int value ) {
 
+		// 열 체크
 		for(int i=0;i<9;i++) {
-			if( v.contains(sdc[i][y]) ) {
-				return false;
-			}
-			else {
-				v.add(sdc[i][y]);
-			}
+			if(arr[row][i] == value) return false;
 		}
-		return true;
 
-	}
-
-	private static boolean columnCheck(int [][] sdc, int x, int y) {
-		Vector<Integer> v=new Vector<>();
-
+		// 행 체크
 		for(int i=0;i<9;i++) {
-			if( v.contains(sdc[x][i]))
-				return false;
-
-			else
-				v.add(sdc[x][i]);
+			if(arr[i][col] == value) return false;
 		}
+
+		// 3 x 3 체크
+		int set_row= (row/3) * 3 ;
+		int set_col= (col/3) * 3 ;
+
+		for(int i=set_row; i< set_row+3 ;++ i) {
+			for(int j= set_col; j<set_col+3; ++j)
+				if(arr[i][j]==value) return false;
+		}
+
 		return true;
+
 
 	}
 
-	private static boolean squareCheck(int [][] sdc, int i, int j) {
-		Vector<Integer> v=new Vector<>();
+	public static void sudoku(int row, int col) {
 
-		if(i>=0 && i<3 ) i=0;
-		else if(i<6) i=3;
-		else i=6;
+		// 해당 행이 다 채워지면 다음행의 첫번째부터 다시 시 작
+		if(col==9) {
+			sudoku(row+1,0);
+			return;
+		}
 
-		if(j>=0 && j<3 ) j=0;
-		else if(j<6) j=3;
-		else j=6;
-
-		for(int x=0;x<3;x++) {
-			for(int y=0;y<3;y++) {
-				if( v.contains(sdc[i+x][j+y]) ) return false;
-				else v.add(sdc[i+x][j+y]);
+		// 행과 열이 모두 다 채워졌으면 출력 후 종료
+		if(row==9) {
+			for(int i=0;i<9;i++) {
+				for(int j=0;j<9;j++) {
+					System.out.print(arr[i][j]+" ");
+				}
+				System.out.println();
 			}
+
+			System.exit(0);
 		}
 
-		return true;
+		if(arr[row][col]==0) {
+			for(int i=1;i<10;++i) {
+
+				if(possibility(row,col,i)) {
+					arr[row][col]=i;
+					sudoku(row,col+1);
+				}
+			}
+			arr[row][col]=0;
+			return;
+		}
+
+		sudoku(row,col+1);
+
+
 
 	}
-	private static Vector<Integer> remain(int [][] sdc, int i, int j){
-		boolean [] tmp=new boolean[10];
-		Vector<Integer> v=new Vector<>();
 
-		for(int x=0;x<9;x++) {
-			int c=sdc[i][x];
-			tmp[c]=true;
-		}
-
-		for(int x=1;x<tmp.length;x++) {
-			if(tmp[x]==false) v.add(x);
-		}
-
-		return v;
-
-	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		int [][] sdc=new int [9][9];
 		for(int i = 0; i < 9; i ++) {
 			for( int j = 0; j < 9 ; j ++)
-				sdc[i][j]=sc.nextInt();
+				arr[i][j]=sc.nextInt();
 		}
 
-		for(int i = 0; i < 9; i ++) {
-			for( int j = 0; j < 9 ; j ++)
-				if(sdc[i][j]==0) { // 0이면
-					Vector<Integer> v=remain(sdc,i,j);
-					System.out.println(v);
-					for(int k=0;k<v.size();k++) {
-						int c=v.get(k);
-						System.out.println(c);
-						sdc[i][j]=c;
-						if(rowCheck(sdc,i,j)|| columnCheck(sdc,i,j) || squareCheck(sdc,i,j) ) break;
-						else sdc[i][j]=0;
-					}
-				}
 
-		}
+		sudoku(0,0);
 
-		for(int i = 0; i < 9; i ++) {
-			for( int j = 0; j < 9 ; j ++) {
-				System.out.print(sdc[i][j]+" ");
-			}
-			System.out.println();
-
-		}
 
 	}
 
