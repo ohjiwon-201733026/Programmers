82579a7b33a22787e6e12387445cbbbab1e5a5b0
OH JIWON <ojw970725@gmail.com>
2021-01-18 14:07:39 +0900

    210118

---
+++ src/BaekJoon/InOutput/B2558.java
@@ -0,0 +1,16 @@
+// A+B 2
+package BaekJoon.InOutput;
+
+import java.util.Scanner;
+
+public class B2558 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int A=sc.nextInt();
+		int B=sc.nextInt();
+		System.out.println(A+B);
+	}
+
+}
