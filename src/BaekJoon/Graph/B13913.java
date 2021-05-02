973cfffa0505e25121def5bf04d1cda72b796868
OH JIWON <ojw970725@gmail.com>
2021-05-02 19:59:16 +0900

    210502 이모티콘

--- src/BaekJoon/Graph/B13913.java
+++ src/BaekJoon/Graph/B13913.java
@@ -1,9 +1,83 @@
 package BaekJoon.Graph;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class B13913 {
+
+	public static int N, K;
+	public static boolean[] visit;
+	public static int[] parent; // 이전 위치를 저장함
+
+	public static class Pair {
+		int x;
+		int cnt;
+
+		Pair(int x, int cnt) {
+			this.x = x;
+			this.cnt = cnt;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		K = sc.nextInt();
+		visit = new boolean[100001];
+		parent = new int[100001];
+
+		bfs(N, K);
+
+		Stack<Integer> stack=new Stack<>();
+		stack.push(K);
+		int index=K;
+
+		while(index!=N) {
+			stack.push(parent[index]);
+			index=parent[index];
+		}
+
+		while(!stack.isEmpty()) {
+			System.out.print(stack.pop()+" ");
+		}
+
+	}
+
+	private static void bfs(int n, int k) {
+		// TODO Auto-generated method stub
+		Queue<Pair> q = new LinkedList<>();
+		q.add(new Pair(n, 0));
+		visit[n] = true;
+
+		while (!q.isEmpty()) {
+			Pair p = q.poll();
+
+			if (p.x == k) {
+				System.out.println(p.cnt);
+				return;
+			}
+			// 2*x
+			if (0 <= 2 * p.x && 2 * p.x <= 100000 && !visit[2 * p.x]) {
+				q.offer(new Pair(2 * p.x, p.cnt+1));
+				visit[p.x * 2] = true;
+				parent[p.x*2]=p.x;
+			}
+			// x-1
+			if (0 <= p.x - 1 && p.x - 1 <= 100000 && !visit[p.x - 1]) {
+				q.offer(new Pair(p.x - 1, p.cnt + 1));
+				visit[p.x - 1] = true;
+				parent[p.x-1]=p.x;
+			}
+			// x+1
+			if (0 <= p.x + 1 && p.x + 1 <= 100000 && !visit[p.x + 1]) {
+				q.offer(new Pair(p.x + 1, p.cnt + 1));
+				visit[p.x + 1] = true;
+				parent[p.x+1]=p.x;
+			}
+		}
 
 	}
 
---
+++ src/BaekJoon/Graph/B14226.java
@@ -0,0 +1,58 @@
+// 이모티콘
+package BaekJoon.Graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class B14226 {
+	public static class Node{
+		int len; // 화면 이모티콘 길이
+		int buf; // 클립보드의 이모티콘 길이
+		int cnt; // 연산 초
+
+		Node(int len,int buf, int cnt){
+			this.len=len;
+			this.buf=buf;
+			this.cnt=cnt;
+		}
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int S=sc.nextInt();
+
+		boolean [][] visited=new boolean[2002][2002]; //???
+		Queue<Node> q=new LinkedList<>();
+
+		visited[1][0]=true;
+		q.add(new Node(1,0,0));
+
+		while(!q.isEmpty()) {
+			Node cur=q.poll();
+
+			if(cur.len==S) {
+				System.out.println(cur.cnt);
+				return;
+			}
+
+			// copy
+			q.add(new Node(cur.len,cur.len,cur.cnt+1));
+
+			// paste
+			if(!visited[cur.len+cur.buf][cur.buf] && cur.buf!=0 && cur.len+cur.buf!=0)
+				q.add(new Node(cur.len+cur.buf,0,cur.cnt+1));
+
+			//delete
+			if(cur.len>0 && !visited[cur.len-1][cur.buf]&& cur.len-1<=1000) {
+				visited[cur.len-1][cur.buf]=true;
+				q.add(new Node(cur.len-1,cur.buf,cur.cnt+1));
+			}
+		}
+
+	}
+
+
+}
