dfb4e621b794e3fd95c55c3f05892eb1b5ab5655
OH JIWON <ojw970725@gmail.com>
2021-05-09 21:18:39 +0900

    210509 강의실 배정

--- src/BaekJoon/Greedy/B11000_강의실배정.java
+++ src/BaekJoon/Greedy/B11000_강의실배정.java
@@ -1,26 +1,52 @@
 package BaekJoon.Greedy;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class B11000_강의실배정 {
+	public static class Lecture implements Comparable<Lecture>{
+		int start;
+		int end;
+
+		public Lecture(int start, int end) {
+			this.start=start;
+			this.end=end;
+		}
+
+		@Override
+		public int compareTo(Lecture o1) {
+			// TODO Auto-generated method stub
+			if(o1.start==this.start) {
+				return this.end-o1.end;
+			}
+			return this.start-o1.start;
+		}
+
+
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		int N=sc.nextInt();
-		long [] S=new long[N];
-		long [] T=new long[N];
-		int [] a=new int [N];
+		Lecture [] a=new Lecture [N];
 
 		for(int i=0;i<N;++i) {
-			S[i]=sc.nextLong();
-			T[i]=sc.nextLong();
+			a[i]=new Lecture(sc.nextInt(),sc.nextInt());
 		}
-		int cnt=1;
-		for(int i=0;i<N;++i) {
-			if()
+		Arrays.sort(a);
+		PriorityQueue<Integer> pq=new PriorityQueue<>();
+
+		pq.add(a[0].end);
+		for(int i=1;i<N;++i) {
+			if(a[i].start>=pq.peek()) {
+				pq.poll();
+			}
+			pq.add(a[i].end);
 		}
 
+		System.out.println(pq.size());
 
 	}
 
--- src/Programmers/Kakao/Kakao20210508_2.java
+++ src/Programmers/Kakao/Kakao20210508_2.java
@@ -5,52 +5,38 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Kakao20210508_2 {
-	public static int [][] visit;
-	public static char [][] arr;
-	public static int [] result;
 	public static int [] dx= {0,-1,0,1};
 	public static int [] dy= {-1,0,1,0};
-	public static boolean flag=true;
+	public static char [][] arr=new char[5][5];
+	public static int [][] visit;
+	public static boolean flag;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		int N=5;
-		arr=new char[5][5]; // 입력 받을 배열
-		result=new int [5]; // 결과 담을 배열
-		String [] tmp= {"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"};
-//		while(N-->0) {
-			flag=true;
-			for(int i=0;i<5;++i) { // 입력받기
-				String s=tmp[i];
-				for(int j=0;j<5;++j) {
-					arr[i][j]=s.charAt(j);
-				}
+		String [] tmp= {
+				"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"
+		};
+
+		for(int i=0;i<5;++i) {
+			String s=tmp[i];
+			for(int j=0;j<5;++j) {
+				arr[i][j]=s.charAt(j);
 			}
+		}
 
-			for(int i=0;i<5;++i) { // 'P'찾기
-				for(int j=0;j<5;++j) {
-					if(arr[i][j]=='P') {
-						visit=new int [5][5];
-						bfs(i,j);
-					}
-				}
-			}
-			System.out.println(flag);
-//		}
-
-	}
-
-	private static void print() {
-		// TODO Auto-generated method stub
 		for(int i=0;i<5;++i) {
 			for(int j=0;j<5;++j) {
-				System.out.print(visit[i][j]);
+				if(arr[i][j]=='P') {
+					visit=new int [5][5];
+					bfs(i,j);
+				}
 			}
-			System.out.println();
 		}
+
 		System.out.println(flag);
-		System.out.println();
+
+
 
 	}
 
@@ -58,7 +44,7 @@ public class Kakao20210508_2 {
 		int x;
 		int y;
 
-		Node (int x, int y){
+		public Node(int x,int y) {
 			this.x=x;
 			this.y=y;
 		}
@@ -69,30 +55,27 @@ public class Kakao20210508_2 {
 		Queue<Node> q=new LinkedList<>();
 		q.add(new Node(i,j));
 		visit[i][j]=1;
-		loop1:
+
 		while(!q.isEmpty()) {
 			Node p=q.poll();
-			for(int k=0;k<4;++k) {
-				int x=p.x+dx[k];
-				int y=p.y+dy[k];
-				if(0<=x && x<5 && 0<=y && y<5) { // 범위 안에 있고
-					if(arr[x][y]=='O') { // 빈칸 만났을 경우
-						visit[x][y]=visit[p.x][p.y]+1;
-						q.add(new Node(x,y));
-					}
-					if(arr[x][y]=='X') { // 파티션인 경우 4
-						visit[x][y]=4;
-						q.add(new Node(x,y));
-					}
-					if(arr[x][y]=='P') { // 다른 응시자 있을 경우
-						if(visit[p.x][p.y]+1<5) { // 3칸 띄어지지 않는 경우
-							flag=false; // false
-							q.add(new Node(x,y));
-							break loop1;
-					}
-				}
 
-			}
+			for(int k=0;k<4;++k) {
+				int x= p.x+dx[k];
+				int y= p.y+dy[k];
+
+				if(x>=0 && x<5 && y>=0 && y<5) {
+					if(arr[x][y]=='O') {
+						visit[x][y]=visit[p.x][p.y]+1;
+					}
+					if(arr[x][y]=='X')
+						visit[x][y]=4;
+					if(arr[x][y]=='P') {
+						visit[x][y]=visit[p.x][p.y]+1;
+						flag=false;
+					}
+
+					q.add(new Node(x,y));
+				}
 			}
 		}
 
