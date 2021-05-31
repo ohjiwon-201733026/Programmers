374adbc91edcbc946feb9d0ecb17bd96b3d0402d
OH JIWON <ojw970725@gmail.com>
2021-05-31 14:01:44 +0900

    210531 four squares

---
+++ src/BaekJoon/DynamicProgramming/B17626_FourSquares.java
@@ -0,0 +1,34 @@
+package BaekJoon.DynamicProgramming;
+
+import java.util.Scanner;
+
+public class B17626_FourSquares {
+
+	public static int[] dp;
+	public static int N;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		dp = new int[N + 1];
+		dp[0] = 0;
+		dp[1] = 1;
+		recur(N);
+		System.out.println(dp[N]);
+
+	}
+
+	private static void recur(int n) {
+		// TODO Auto-generated method stub
+		for (int i = 2; i <= N; ++i) {
+			int min = Integer.MAX_VALUE;
+			for (int j = 1; j * j <= i; j++) {
+				min = Math.min(min, dp[i - j * j]);
+			}
+			dp[i]=min+1;
+		}
+
+	}
+
+}
