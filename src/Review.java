f56b793af4b43126717c2a444aeca356b0a4d717
OH JIWON <ojw970725@gmail.com>
2021-05-31 15:01:34 +0900

    210531 그리디 복습

---
+++ src/Review.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Review {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+
+		if(s.equals("X")) {
+			System.out.println(-1);
+			return;
+		}
+
+	}
+}
