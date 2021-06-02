327cbd62e8115aa4774defb7776b28b918d56823
OH JIWON <ojw970725@gmail.com>
2021-06-02 23:35:32 +0900

    210602

--- src/BaekJoon/Greedy/B8980_택배.java
+++ src/BaekJoon/Greedy/B8980_택배.java
@@ -1,6 +1,5 @@
 package BaekJoon.Greedy;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -19,10 +18,10 @@ public class B8980_택배 {
 		@Override
 		public int compareTo(Box o) {
 			// TODO Auto-generated method stub
-			if(this.start==o.start) {
-				return this.end-o.end;
+			if(this.end==o.end) {
+				return this.start-o.start;
 			}
-			return this.start-o.start;
+			return this.end-o.end;
 		}
 
 
@@ -34,49 +33,46 @@ public class B8980_택배 {
 		int N=sc.nextInt(); // 마을 수
 		int C=sc.nextInt(); // 트럭 용량
 		int M=sc.nextInt(); // 박스 수
-		Box [] arr=new Box[M];
-		ArrayList<Box> truck=new ArrayList<>();
-		int cur=0;// 현재 트럭에 실은 중량
-		int sum=0; // 총 옮긴 박스 중량
 
-		for(int i=0;i<M;++i) {
-			arr[i]=new Box(sc.nextInt(),sc.nextInt(),sc.nextInt());
+		Box [] del=new Box[M+1];
+		for(int i=1;i<=M;++i) {
+			int start=sc.nextInt();
+			int end=sc.nextInt();
+			int weight=sc.nextInt();
+
+			del[i]=new Box(start,end,weight);
 		}
-		Arrays.sort(arr);
+		Arrays.sort(del,1,M+1);
 
-		for(int i=1;i<=N;++i) { // 마을 방문
-
-			// i마을 배송지에 배송하기
-			for(int j=0;j<truck.size();++j) {
-				if(truck.get(j).end==i) {
-					cur-=truck.get(j).weight; // 트럭에서 내리기
-					truck.remove(j);
-				}
-			}
-
-			// 트럭에 실기
-			for(int k=0;k<arr.length;++k) { // 박스 순환
-				if(cur>C) break;
-				if(arr[k].start==i) { // 출발지이면
-					if(arr[k].weight+cur<C) { // 실어서 트럭 용량
-						cur+=arr[k].weight;
-						truck.add(new Box(arr[k].start,arr[k].end,arr[k].weight));
-						sum+=arr[k].weight;
-					}
-					else {
-						int w=C-cur;
-						cur+=w;
-						truck.add(new Box(arr[k].start,arr[k].end,w));
-						sum+=w;
-
-					}
-				}
-			}
-
-
+		int [] weight=new int [N+1];
+		for(int i=1;i<N;++i) {
+			weight[i]=C;
 		}
 
-		System.out.println(sum);
+		int ans=0;
+		for(int i=1;i<=M;++i) {
+			Box b=del[i];
+
+			int maxBoxNum=Integer.MAX_VALUE;
+			for(int j=b.start;j<b.end;++j) {
+				maxBoxNum=Math.min(maxBoxNum, weight[j]);
+			}
+
+			if(maxBoxNum>=b.weight) {
+				for(int j=b.start;j<b.end;++j) {
+					weight[j]-=b.weight;
+				}
+				ans+=b.weight;
+			}
+			else {
+				for(int j=b.start;j<b.end;++j) {
+					weight[j]-=maxBoxNum;
+				}
+				ans+=maxBoxNum;
+			}
+		}
+
+		System.out.println(ans);
 
 	}
 
