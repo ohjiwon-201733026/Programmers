821b089c3e237bb8998b107fc8d0d5123b155c78
OH JIWON <ojw970725@gmail.com>
2021-06-04 12:03:25 +0900

    210604 6월1주차 스터디 과제

---
+++ src/study/s6_1/위장.java
@@ -0,0 +1,50 @@
+package study.s6_1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class 위장 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String [][] clothes= {{"yellowhat", "headgear"},{"bluesunglasses", "eyewear"},{"green_turban", "headgear"}};
+		int result=solution(clothes);
+		System.out.println(result);
+	}
+
+	private static int solution(String[][] clothes) {
+		// TODO Auto-generated method stub
+		Map<String,ArrayList<String>> map=new HashMap<>();
+
+		for(int i=0;i<clothes.length;++i) {
+			String [] a=clothes[i];
+			if(map.containsKey(a[1])) {
+				ArrayList<String> arr=map.get(a[1]);
+				arr.add(a[0]);
+			}
+			else {
+				ArrayList<String> arr=new ArrayList<>();
+				arr.add(a[0]);
+				map.put(a[1], arr);
+			}
+		}
+
+		int m=map.size();
+		int answer=0;
+		int tmp=1;
+		for(Map.Entry<String,ArrayList<String>> entry : map.entrySet()) {
+			if(m==1) {
+				answer=entry.getValue().size();
+				return answer;
+			}
+			else {
+				tmp*=(entry.getValue().size()+1);
+			}
+
+		}
+		answer=tmp-1;
+		return answer;
+	}
+
+}
---
+++ src/study/s6_1/큰수만들기.java
@@ -0,0 +1,48 @@
+package study.s6_1;
+
+import java.util.Stack;
+
+public class 큰수만들기 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String number="4177252841";
+		int k=4;
+		String answer=solution(number,k);
+		System.out.println(answer);
+	}
+
+	private static String solution(String number, int k) {
+		// TODO Auto-generated method stub
+		int [] num=new int[number.length()];
+		for(int i=0;i<number.length();++i) {
+			num[i]=number.charAt(i)-'0';
+		}
+		Stack<Integer> stack=new Stack<>();
+		stack.push(num[0]);
+		int i=1;
+		for(i=1;i<num.length;++i) {
+
+			while(k>0 && !stack.isEmpty() && stack.peek()<num[i]) {
+				stack.pop();
+				k--;
+			}
+			stack.push(num[i]);
+		}
+
+		System.out.println(stack);
+		StringBuilder sb=new StringBuilder();
+		Stack<Integer> tmp=new Stack<>();
+		while(!stack.isEmpty()) {
+			tmp.add(stack.pop());
+		}
+
+		while(!tmp.isEmpty()) {
+			sb.append(tmp.pop());
+		}
+
+		return sb.toString();
+
+	}
+
+}
