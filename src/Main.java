2f54f495ce11b51cf08302c590c4c7ae1784923d
OH JIWON <ojw970725@gmail.com>
2021-06-10 18:31:03 +0900

    210610 스터디 5주차 과제

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,26 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
 	public static int [] dp;
-
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n=sc.nextInt();
-		dp= new int [n+1];
-		System.out.println(dp(n));
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+
+		while(T-->0){
+			int n=sc.nextInt();
+			dp=new int [n+1];
+			System.out.println(recur(n));
+		}
+
+
 	}
 
-	private static int dp(int n) {
-		if(n==1) return 0;
-		if(n==2) return 1;
+	private static int recur(int n) {
+		if(n==1) return dp[1]=1;
+		if(n==2) return dp[2]=2;
+		if(n==3) return dp[3]=4;
 
-		if(n%3==0) return dp[n]=dp(n/3)+1;
-		if(n%2==0) return dp[n]=dp(n/2)+1;
-		dp[n]=dp[n-1]+1;
-		return dp[n];
+		return dp[n]=recur(n-3)+recur(n-2)+recur(n-1);
 	}
-
 
 }
