59197abd7ba37cce7682423e2a35cea8c677a995
OH JIWON <ojw970725@gmail.com>
2021-07-18 12:28:04 +0900

    210718 String 백준

--- src/BaekJoon/Strings/B1764_듣보잡.java
+++ src/BaekJoon/Strings/B1764_듣보잡.java
@@ -3,34 +3,31 @@ package BaekJoon.Strings;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class B1764_듣보잡 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        String s=br.readLine();
-        String [] arr=s.split(" ");
-        int N=Integer.parseInt(arr[0]);
-        int M=Integer.parseInt(arr[1]);
-
-        ArrayList<String> notListen=new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        HashSet<String> notListen=new HashSet<>();
+        ArrayList<String> ans=new ArrayList<>();
 
         for(int i=0;i<N;++i){
             String name=br.readLine();
             notListen.add(name);
         }
         int cnt=0;
-        StringBuilder sb=new StringBuilder();
+
         for(int i=0;i<M;++i){
             String name=br.readLine();
-            if(notListen.contains(name)) { cnt++; sb.append(name+"\n"); }
+            if(notListen.contains(name)) { ans.add(name); }
         }
-        String answer=cnt+"\n"+sb.toString();
-
-        System.out.println(answer);
-
+        Collections.sort(ans);
+        System.out.println(ans.size());
+        for(String s: ans) System.out.println(s);
 
     }
 }
