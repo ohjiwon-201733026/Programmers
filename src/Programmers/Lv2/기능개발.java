a1096f13cb29a8ce01370fb83742e0f1b05d94c3
OH JIWON <ojw970725@gmail.com>
2021-07-27 14:26:20 +0900

    210727 프로그래머스 Lv2 다시풀기

--- src/Programmers/Lv2/기능개발.java
+++ src/Programmers/Lv2/기능개발.java
@@ -1,6 +1,7 @@
 package Programmers.Lv2;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class 기능개발 {
 
@@ -14,30 +15,33 @@ public class 기능개발 {
     }
 
     private static int[] solution(int[] progresses, int[] speeds) {
+        ArrayList<Integer> answer=new ArrayList<>();
+        int [] day=new int [progresses.length];
 
-        int [] mod=new int [progresses.length];
-
-        for(int i=0;i<mod.length;++i){
-            mod[i]=(int)(Math.ceil((double)(100-progresses[i])/(speeds[i])));
+        for(int i=0;i< progresses.length;++i){
+            day[i]=(int)(Math.ceil((double) (100-progresses[i])/speeds[i]));
         }
-        ArrayList<Integer> ans=new ArrayList<>();
-        int pre=mod[0];
+
+       Stack<Integer> stack=new Stack<>();
+        stack.add(day[0]);
         int cnt=1;
-        if(mod.length>1) {
-            for (int i = 1; i < mod.length; ++i) {
-                if (pre >= mod[i]) cnt++;
-                else {
-                    ans.add(cnt);
-                    cnt = 1;
-                    pre = mod[i];
-                }
+        for(int i=1;i<day.length;++i){
+            if(stack.peek() >= day[i]) cnt++;
+            else {
+                answer.add(cnt);
+                stack.pop();
+                cnt=1;
+                stack.push(day[i]);
             }
-            ans.add(cnt);
         }
-        else ans.add(1);
-        int [] answer=new int [ans.size()];
-        for(int i=0;i<ans.size();++i) answer[i]=ans.get(i);
+        answer.add(cnt);
 
-        return answer;
+        int [] ans=new int[answer.size()];
+
+        for(int i=0;i< answer.size();++i) ans[i]=answer.get(i);
+
+
+        return ans;
+
     }
 }
--- src/Programmers/Lv2/행렬테두리회전하기.java
+++ src/Programmers/Lv2/행렬테두리회전하기.java
@@ -7,7 +7,7 @@ public class 행렬테두리회전하기 {
         int [][]queries={
                 {2,2,5,4},
                 {3,3,6,6},
-//                {5,1,6,3}
+                {5,1,6,3}
         };
         int [] result=solution(rows, columns, queries);
         for(int a:result) System.out.println(a);
@@ -15,46 +15,47 @@ public class 행렬테두리회전하기 {
     }
 
     public static int[] solution(int rows, int columns, int[][] queries) {
-        int[] answer = new int [queries.length];
-        int [][] map=new int [rows+1][columns+1];
-
-        for(int i=1;i<rows+1;++i){
-            for(int j=1;j<columns+1;++j){
-                map[i][j]=(i-1)*columns+j;
+       int [] answer=new int [queries.length];
+       int [][] map=new int [rows+1][columns+1];
+        // map 채우기
+       for(int i=1;i<=rows;++i){
+           for(int j=1;j<=columns;++j){
+               map[i][j]=(i-1)*columns+j;
+           }
+       }
+        int [][] change=new int [rows+1][columns+1];
+        for(int i=1;i<=rows;++i){
+            for(int j=1;j<=columns;++j){
+                change[i][j]=map[i][j];
             }
         }
-        int [][] ch=new int [rows+1][columns+1];
-        for(int i=1;i<rows+1;++i){
-            for(int j=1;j<columns+1;++j){
-                ch[i][j]=map[i][j];
-            }
-        }
-        for(int k=0;k<queries.length;++k){
-            int [] q=queries[k];
-            int x1=q[0],y1=q[1],x2=q[2],y2=q[3];
-            int dx=x2-x1, dy=y2-y1;
-            int min=Integer.MAX_VALUE;
-            for(int i=1;i<rows+1;++i){
-                for(int j=1;j<columns+1;++j){
-                    map[i][j]=ch[i][j];
-                }
-            }
-            for(int i=y1;i<y1+dy;i++) { ch[x1][i+1]=map[x1][i]; min=Math.min(map[x1][i],min);}
-            for(int i=x1;i<x1+dx;++i) { ch[i+1][y2]=map[i][y2]; min=Math.min(map[i][y2],min);}
-            for(int i=y1+1;i<=y1+dy;++i) { ch[x2][i-1]=map[x2][i]; min=Math.min(map[x2][i],min);}
-            for(int i=x1+1;i<=x1+dx;++i)  {ch[i-1][y1]=map[i][y1]; min=Math.min(map[i][y1],min);}
-            answer[k]=min;
-        }
 
-        for(int i=1;i<rows+1;++i){
-            for(int j=1;j<columns+1;++j){
-                System.out.print(ch[i][j]+" ");
-            }
-            System.out.println();
-        }
+       for(int k=0;k< queries.length;++k){
+           
+           // 복사
+           for(int i=1;i<=rows;++i){
+               for(int j=1;j<=columns;++j){
+                   map[i][j]=change[i][j];
+               }
+           }
 
+           // 옮기기
+           int x1=queries[k][1],y1=queries[k][0],x2=queries[k][3],y2=queries[k][2];
+           int min=Integer.MAX_VALUE;
+           for(int i=x1;i<x2;++i){ change[y1][i+1]=map[y1][i]; min=Math.min(min,map[y1][i]);}
+           for(int i=y1;i<y2;++i){ change[i+1][x2]=map[i][x2]; min=Math.min(min,map[i][x2]);}
+           for(int i=x2;i>x1;--i){ change[y2][i-1]=map[y2][i]; min=Math.min(min,map[y2][i]);}
+           for(int i=y2;i>y1;--i){ change[i-1][x1]=map[i][x1]; min=Math.min(min,map[i-1][x1]);}
 
+           answer[k]=min;
 
-        return answer;
+           for(int i=1;i<=rows;++i){
+               for(int j=1;j<=columns;++j){
+                   System.out.print(change[i][j]+" ");
+               }
+               System.out.println();
+           }
+       }
+       return answer;
     }
 }
