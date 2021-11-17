012c3a5c68b97fd2129898567c1257dd4ffa99a6
OH JIWON <ojw970725@gmail.com>
2021-11-17 17:48:36 +0900

    211117

--- src/Yogiyo1.java
+++ src/Yogiyo1.java
@@ -3,48 +3,5 @@ import java.util.Locale;
 
 public class Yogiyo1 {
 
-    public static void main(String[] args) {
-        String S="John Doe; John A Doe; John B Doe; John Doe; John Evan Doe; Jane Doe; John Doe";
-        String C="Example";
 
-        String answer=solution(S,C);
-        System.out.println(answer);
-    }
-
-    private static String solution(String S, String C) {
-        String [] names=S.split("; ");
-        StringBuilder answer=new StringBuilder();
-        HashMap<String,Integer> map=new HashMap<>();
-
-        for(int i=0;i< names.length;++i){
-            StringBuilder tmp=new StringBuilder();
-            String lower_name=names[i].toLowerCase();
-            String [] nameArr=lower_name.split(" ");
-            tmp.append(nameArr[0]);
-            String lastName=nameArr[nameArr.length - 1].replaceAll("-", "");
-            if(nameArr[nameArr.length-1].length()>8) {
-                lastName = lastName.substring(0, 8);
-            }
-
-            tmp.append("." + lastName + "@");
-            tmp.append(C.toLowerCase() + ".com");
-
-            if(map.containsKey(tmp.toString())){
-                String s=tmp.toString();
-                int n=map.get(tmp.toString());
-                tmp=new StringBuilder();
-                n++;
-                tmp.append(nameArr[0]+"."+lastName+n+"@"+C.toLowerCase()+".com");
-                map.put(s,n);
-            }
-            else{
-                map.put(tmp.toString(),1);
-            }
-            if(i== names.length-1) answer.append(tmp);
-            else answer.append(tmp+"; ");
-
-
-        }
-        return answer.toString();
-    }
 }
--- src/Yogiyo3.java
+++ src/Yogiyo3.java
@@ -1,42 +1,4 @@
 public class Yogiyo3 {
 
-    public static void main(String[] args) {
-        int N=44432;
-        int answer= solution(N);
-        System.out.println(answer);
-    }
 
-    public static int check(int n){
-        String s=Integer.toString(n);
-        char pre=s.charAt(s.length()-1);
-        for(int i=s.length()-2;i>=0;--i){
-            if(pre==s.charAt(i)) return i;
-            else pre=s.charAt(i);
-        }
-
-        return -1;
-    }
-
-    private static int solution(int N) {
-        N+=1;
-
-        while(true){
-            if(check(N)==-1) break;
-            int idx=check(N)+1;
-            String s=Integer.toString(N);
-            if(idx==s.length()-1) N++;
-            else {
-                int change = s.charAt(idx) - '0';
-                change++;
-                if (change == 10) idx--;
-                StringBuilder sb = new StringBuilder(s.substring(0, idx) + change);
-
-                for (int i = idx; i < s.length(); ++i) sb.append("0");
-                N = Integer.parseInt(sb.toString());
-            }
-
-        }
-
-        return N;
-    }
 }
