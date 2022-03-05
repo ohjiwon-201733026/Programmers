7ffc4698db36480a48a3a7607c317332057f8576
OH JIWON <ojw970725@gmail.com>
2022-03-05 23:15:18 +0900

    220304

---
+++ src/programmers2/level3/네트워크.java
@@ -0,0 +1,55 @@
+package programmers2.level3;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class 네트워크 {
+
+    @Test
+    public void test(){
+        Assertions.assertEquals(2,solution(3,new int [][]{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}}));
+        Assertions.assertEquals(1,solution(3,new int [][]{{1, 1, 0}, {1, 1, 1}, {0, 1, 1}}));
+    }
+
+    static boolean[] visited;
+    static int n;
+    static HashMap<Integer, ArrayList<Integer>> connect=new HashMap<>();
+    public int solution(int n, int [][] computers) {
+        visited=new boolean[n];
+        this.n=n;
+        int answer=0;
+        for(int i=0;i<n;++i){
+            ArrayList<Integer> tmp=new ArrayList<>();
+            for(int j=0;j<n;++j){
+                if(i!=j){
+                    if(computers[i][j]==1) tmp.add(j);
+                }
+            }
+            connect.put(i,tmp);
+        }
+
+        for(int i=0;i<n;++i){
+            if(!visited[i]){
+                answer++;
+                visited[i]=true;
+                dfs(i);
+            }
+        }
+        return answer;
+    }
+
+    private void dfs(int i){
+        ArrayList<Integer> list=connect.get(i);
+        for (Integer j : list) {
+            if(!visited[j]){
+                visited[j]=true;
+                dfs(j);
+            }
+        }
+    }
+
+
+}
--- src/programmers2/level3/디스크컨트롤러.java
+++ src/programmers2/level3/디스크컨트롤러.java
@@ -14,36 +14,34 @@ public class 디스크컨트롤러 {
     }
 
     public int solution(int [][] jobs){
-        int answer=0;
-        int end=0; // 수행 직후 시간
-        int jobsIdx=0; // jobs 배열의 인덱스
-        int count=0; // 수행된 요청의 갯수
+       int answer=0;
+       int n= jobs.length;
+       int end=0;
+       int jobsIdx=0;
+       int count=0;
 
-        // 원본 배열 오름차순 정렬( 요청 시간 오름차순 )
-        Arrays.sort(jobs,(o1,o2)->o1[0]-o2[0]);
+       // 도착 시간 오름차순 정렬
+       Arrays.sort(jobs,(o1,o2)->o1[0]-o2[0]);
 
-        // 처리 시간 오름차순으로 정렬하는 우선순위 큐
-        PriorityQueue<int []> pq=new PriorityQueue<>((o1,o2)->o1[1]-o2[1]);
+       // 작업 시간 오름차순 정렬
+       PriorityQueue<int []> pq=new PriorityQueue<>((o1,o2)->o1[1]-o2[1]);
 
-        // 요청이 모두 수행될 때까지 반복
-        while (count<jobs.length){
-            // 하나의 작업이 완료되는 시점까지 들어온 모든 요청을 큐에 넣음
-            while (jobsIdx<jobs.length && jobs[jobsIdx][0]<=end)
-                pq.add(jobs[jobsIdx++]);
+       while (count<n){
 
-            // 큐가 비어있다면 작업 완료 이후에 다시 요청이 들어온다는 의미
-            // end를 요청의 가장 처음으로 맞춰줌
-            if(pq.isEmpty()){
-                end=jobs[jobsIdx][0];
-            }
-            else {
-                int [] tmp=pq.poll();
-                answer+=tmp[1]+end-tmp[0];
-                end+=tmp[1];
-                count++;
-            }
-        }
+           while (jobsIdx<n && jobs[jobsIdx][0]<=end){
+                   pq.add(jobs[jobsIdx++]);
+           }
+           if(pq.isEmpty()){
+               end=jobs[jobsIdx][0];
+           }
+           else{
+               int [] tmp=pq.poll();
+               answer+=(end+tmp[1])-tmp[0];
+               end+=tmp[1];
+               count++;
+           }
+       }
 
-        return answer/jobs.length;
+       return answer/n;
     }
 }
